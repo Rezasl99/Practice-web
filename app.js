581a2e7eeabe4025463d72b3c481d9f64989e50f
Rezasl99 <Rezasoltani78@gmail.com>
2023-07-12 17:07:56 +0430

    12/7/23

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const Posts = []
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -35,9 +36,13 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose" , (req, res) => {
-  let post = req.body.post
-  console.log(post);
-  res.redirect("/compose");
+  let post = {
+    title : req.body.postTitle,
+    content : req.body.postBody
+  }
+  Posts.push(post.title,post.content);
+  console.log(Posts);
+  res.redirect("/");
 })
 
 app.listen(3000, function () {
